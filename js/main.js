a201f74f2d17f1896d5dbb036b8411a9f365d4c3
AlexanderCherednicheenko <acherednichenko174@gmail.com>
2021-10-08 23:56:48 +0300

    создание файла media.css
    Создание адаптивной версии сайта.
    Замена логотипа на svg спрайт

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-const navLink = document.querySelectorAll('.header__menu-nav a');
+const menu = () => {
+   const menuBtn = document.querySelector('.burger-menu');
+   const menu = document.querySelector('.header__menu-nav');
+   const navLink = document.querySelectorAll('.header__menu-nav a');
+   const body = document.querySelector('body');
+   const logo = document.querySelector('#logo-icon');
+   const language = document.querySelector('.language');
 
-navLink.forEach((elem) => {
-   elem.addEventListener('click', (event) => {
-      event.preventDefault();
-      const id = event.target.getAttribute('href').substr(1);
+   menuBtn.addEventListener('click', () => {
+      menuBtn.classList.toggle('active');
+      menu.classList.toggle('active');
+      logo.classList.toggle('active');
+      body.classList.toggle('lock');
+      language.classList.toggle('active');
+   });
 
-      document.getElementById(id).scrollIntoView({
-         behavior: 'smooth',
-         block: 'start'
+   navLink.forEach((elem) => {
+      elem.addEventListener('click', (event) => {
+         event.preventDefault();
+         const id = event.target.getAttribute('href').substr(1);
+         document.getElementById(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         });
+         menuBtn.classList.remove('active');
+         menu.classList.remove('active');
+         logo.classList.remove('active');
+         body.classList.remove('lock');
+         language.classList.remove('active');
       });
    });
-});
+};
+menu();
